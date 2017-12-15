533d4fe6abc348f7c6765f6cf0acce4974f4e687
buxizhizhoum <mapeaks@126.com>
2017-12-15 14:58:50 +0800

    Change training times

--- tf/exercise_mnist.py
+++ tf/exercise_mnist.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 learning_rate = 0.001
-train_times = 30000
+train_times = 10000
 
 mnist_data_sets = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
