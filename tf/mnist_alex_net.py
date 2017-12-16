5247a637a33d49cb648f8552d9712c7aa0531890
buxizhizhoum <mapeaks@126.com>
2017-12-16 11:16:57 +0800

    mnist_alex_net test passed.

--- tf/mnist_alex_net.py
+++ tf/mnist_alex_net.py
@@ -41,38 +41,38 @@ def alex_net(x, weights, biases, drop_out):
     norm_2 = norm("norm_2", pool_2, l_size=4)
 
     # 3rd layer
-    conv_3 = conv_2d("conv_3", conv_2, weights["wc3"], biases["bc3"])
+    conv_3 = conv_2d("conv_3", norm_2, weights["wc3"], biases["bc3"])
     pool_3 = max_pool_2d("pool_3", conv_3, k=2)
     norm_3 = norm("norm_3", pool_3, l_size=4)
 
     # 4th layer
-    conv_4 = conv_2d("conv_4", conv_3, weights["wc4"], biases["bc4"])
+    conv_4 = conv_2d("conv_4", norm_3, weights["wc4"], biases["bc4"])
     pool_4 = max_pool_2d("pool_4", conv_4, k=2)
     norm_4 = norm("norm_4", pool_4, l_size=4)
 
     # 5th layer
-    conv_5 = conv_2d("conv_5", conv_4, weights["wc5"], biases["bc5"])
+    conv_5 = conv_2d("conv_5", norm_3, weights["wc5"], biases["bc5"])
     pool_5 = max_pool_2d("pool_5", conv_5, k=2)
     norm_5 = norm("norm_5", pool_5, l_size=4)
 
     # full connect layer
-    fc1 = tf.reshape(norm_5, [-1, weights["wd1"].get_shape().aslist()[0]])
-    fc1 = tf.add(tf.matmul(fc1, weights["wd1"], biases["bd1"]))
+    fc1 = tf.reshape(norm_5, [-1, weights["wd1"].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights["wd1"]), biases["bd1"])
     fc1 = tf.nn.relu(fc1)
 
     # dropout
     fc1 = tf.nn.dropout(fc1, drop_out)
 
     # full connect layer 2
-    fc2 = tf.reshape(fc1, [-1, weights["wd1"].get_shape().aslist()[0]])
-    fc2 = tf.add(tf.matmul(fc2, weights["wd1"], biases["bd1"]))
+    fc2 = tf.reshape(fc1, [-1, weights["wd1"].get_shape().as_list()[0]])
+    fc2 = tf.add(tf.matmul(fc2, weights["wd1"]), biases["bd1"])
     fc2 = tf.nn.relu(fc2)
 
     # dropout
     fc2 = tf.nn.dropout(fc2, drop_out)
 
     # output layer
-    out = tf.add(tf.matmul(fc2, weights["out"], biases["out"]))
+    out = tf.add(tf.matmul(fc2, weights["out"]), biases["out"])
     return out
 
 
@@ -117,33 +117,34 @@ if __name__ == "__main__":
     # build model
     predict = alex_net(x, weights, biases, keep_prob)
     # loss function and optimizer
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(predict, y))
+    cost = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits=predict, labels=y))
     optimizer = tf.train.AdamOptimizer(
         learning_rate=learning_rate).minimize(cost)
 
-    correct_prediction = tf.equal(tf.arg_max(predict, 1), tf.arg_max(y, 1))
+    correct_prediction = tf.equal(tf.argmax(predict, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     # below is to train and evaluate model
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-        step = 1
+        step = 0
         while step * batch_size < training_iter:
             batch_x, batch_y = mnist.train.next_batch(batch_size)
             sess.run(optimizer,
                      feed_dict={x: batch_x, y: batch_y, keep_prob: dropout})
-
+            print(step)
             if step % display_step == 0:
                 loss, accuracy_training = sess.run([cost, accuracy],
                                                    feed_dict={x: batch_x,
-                                                         y: batch_y,
-                                                         keep_prob: 1})
+                                                   y: batch_y,
+                                                   keep_prob: 1.0})
 
-                print("iter %s, "
-                      "Minibatch loss = %.6f, "
+                print("iter %s, " "Minibatch loss = %.6f, "
                       "Training accurancy = %.6f"
                       % (str(step*batch_size), loss, accuracy_training))
+            step += 1
         print("Training finished!")
 
         test_accuracy = sess.run(accuracy,
