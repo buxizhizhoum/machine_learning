28865a9858127148e62ca9d335dcf6e3a38fa343
buxizhizhoum <mapeaks@126.com>
2017-12-18 16:12:27 +0800

    Add exercise_multiply.py

---
+++ tf/exercise_multiply.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+
+
+a = tf.placeholder("float")
+b = tf.placeholder("float")
+
+res = tf.multiply(a, b)
+
+with tf.Session() as sess:
+    sess.run(res, feed_dict={a: 1.0, b: 2.0})
+    print(sess.run(res, feed_dict={a: 1.0, b: 2.0}))
+
