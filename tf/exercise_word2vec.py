e152cadccc9248a675043026edd75bbcefe1636c
buxizhizhoum <mapeaks@126.com>
2017-12-28 18:04:21 +0800

    test passed version

--- tf/exercise_word2vec.py
+++ tf/exercise_word2vec.py
@@ -1,172 +1,158 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+not very clear about tf.nn.embedding_lookup()
+
+at first, there should be a preprocessor for the data, all of the text is
+transferred to single words,
+the intention to do this is to meet the input
+requirements of tf.nn.embedding_lookup() function.
+
+during the training the data to feed is in type of {input_word, context_word}
+
+
+"""
+import math
+import matplotlib.pyplot as plt
 import numpy as np
+import random
 import tensorflow as tf
 
 from collections import Counter
 
 
-l_rate = 0.01
-n_sample = 100
-embedding_size = 200
+l_rate = 0.01  # learning_rate
+embedding_size = 2  # dimension of vec
+num_sampled = 15    # Number of negative examples to sample.
+batch_size = 20
+
+
+sentences = ["the quick brown fox jumped over the lazy dog",
+            "I love cats and dogs",
+            "we all love cats and dogs",
+            "cats and dogs are great",
+            "sung likes cats",
+            "she loves dogs",
+            "cats can be very independent",
+            "cats are great companions when they want to be",
+            "cats are playful",
+            "cats are natural hunters",
+            "It's raining cats and dogs",
+            "dogs and cats love sung"]
+
+
+def preprocessor(sentences):
+    """
+    preprocessor of sentences
+    :param sentences: sentence in type of list
+    :return:
+    """
+    text = " ".join(sentences)
+    words = text.split(" ")
+    # count the appear times of words {word: appear_times}
+    word_num_dict = Counter(words)
+
+    return words, word_num_dict
+
+words, word_num_dict = preprocessor(sentences)
+# todo extract input, context pair
+unique_words = set(words)
+# set an id number for each word, each number represent for one word
+# 把语料库中的文字整型化,每个整数代表一个单词
+word_2_int = {w: c for c, w in enumerate(unique_words)}
+
+
+def generate_batch(words, size):
+    # get random indexes of word in list words
+    inputs = []
+    labels = []
+    random_index = random.sample(range(len(words)), size//2)
+    for i in random_index:
+        # get one word as input word
+        input_tmp = words[i]
+        # get context of input word
+        if i == 0:
+            label_tmp = [words[i+1]]
+        elif i == len(words) - 1:
+            label_tmp = [words[i-1]]
+        else:
+            label_tmp = [words[i-1], words[i+1]]
+
+        # transfer input word to int number
+        input_int = word_2_int[input_tmp]
+        label_int = [word_2_int[word] for word in label_tmp]
+
+        # align format
+        for i in range(len(label_int)):
+            inputs.append(input_int)  # list
+            labels.append([label_int[i]])  # list of list
+    return inputs, labels
+
+
+vocabulary_size = len(unique_words)
+
+# 一个是一组用整型表示的上下文单词，另一个是目标单词
+# inputs = tf.placeholder(tf.int32, [batch_size], "input")
+# labels = tf.placeholder(tf.int32, [batch_size, 1], "labels")
 
 inputs = tf.placeholder(tf.int32, [None], "input")
 labels = tf.placeholder(tf.int32, [None, None], "labels")
 
-embedding = tf.Variable(
-    tf.random_uniform([vocabulary_size, embedding_size], -1, -1))
-embed = tf.nn.embedding_lookup(embedding, inputs)
+# 定义一个嵌套参数矩阵, 用唯一的随机值来初始化这个大矩阵
+embeddings = tf.Variable(
+    tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+# input should contain the ids used to look up.
+# embeddings are the final word vectors
+embed = tf.nn.embedding_lookup(embeddings, inputs)
 
+# 对语料库中的每个单词定义一个权重值和偏差值
+nce_weights = tf.Variable(
+  tf.truncated_normal([vocabulary_size, embedding_size],
+                      stddev=1.0 / math.sqrt(embedding_size)))
+nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
 
+loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights,
+                                     biases=nce_biases,
+                                     labels=labels,
+                                     inputs=embed,
+                                     num_sampled=num_sampled,
+                                     num_classes=vocabulary_size))
 
-def read_file(filename, size=100000000):
-    """
-    read a file
-    :param filename:
-    :param size: the block size to read
-    :return:
-    """
-    with open(filename, "r") as f:
-        res = f.read(size)
-    return res
+# Construct the SGD optimizer using a learning rate of 1.0.
+optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
 
+# Compute the cosine similarity between minibatch examples and all embeddings.
+# norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+# normalized_embeddings = embeddings / norm
+# valid_embeddings = tf.nn.embedding_lookup(
+#   normalized_embeddings, valid_dataset)
+# similarity = tf.matmul(
+#   valid_embeddings, normalized_embeddings, transpose_b=True)
 
-def preprocessor(text_str, freq=5):
-    """
-    pre processor, delete words that have a very low frequency
-    :param text_str:
-    :param freq:
-    :return:
-    """
-    text_str = text_str.lower()
-    text_str = text_str.replace('.', ' <PERIOD> ')
-    text_str = text_str.replace(',', ' <COMMA> ')
-    text_str = text_str.replace('"', ' <QUOTATION_MARK> ')
-    text_str = text_str.replace(';', ' <SEMICOLON> ')
-    text_str = text_str.replace('!', ' <EXCLAMATION_MARK> ')
-    text_str = text_str.replace('?', ' <QUESTION_MARK> ')
-    text_str = text_str.replace('(', ' <LEFT_PAREN> ')
-    text_str = text_str.replace(')', ' <RIGHT_PAREN> ')
-    text_str = text_str.replace('--', ' <HYPHENS> ')
-    text_str = text_str.replace('?', ' <QUESTION_MARK> ')
-    # text_str = text_str.replace('\n', ' <NEW_LINE> ')
-    text_str = text_str.replace(':', ' <COLON> ')
-    words = text_str.split()
-
-    words_count = Counter(words)
-    # delete words whose frequency is too low
-    trimmed_words = [word for word in words if words_count[word] > freq]
-
-    return trimmed_words
-
-
-def get_target(words, idx, window_size):
-    """
-    get context of words with the idx of word
-    :param words: list of word
-    :param idx: index of input word
-    :param window_size: size of window
-    :return:
-    """
-    start_point = idx - window_size if (idx - window_size) > 0 else 0
-    # when slice a list with a[start: end], if end > len(a), it raise no error
-    end_point = idx + window_size
-
-    res = set(words[start_point: idx]).union(set(words[idx + 1: end_point]))
-    return list(res)
-
-
-def get_batches(words, batch_size, window_size=5):
-    """
-    generator of input_word, output_word pair
-    one input_word to many output_word
-    :param words: list of words
-    :param batch_size:
-    :param window_size:
-    :return:
-    """
-    batch_num = len(words) // batch_size
-    # drop not used item at the tail
-    words_tmp = words[:batch_num * batch_size]
-
-    # lines above is not too important
-    for idx in range(0, len(words_tmp), batch_size):
-        x = []
-        y = []
-        # get a batch
-        batch = words_tmp[idx: idx + batch_size]
-        # get the (input_word, output_word) pair
-        # get input word and get its context
-        for i in range(len(batch)):
-            batch_item_x = batch[i]
-            # this is to get the context of x
-            batch_y = get_target(batch, i, window_size)
-            # align length
-            x.extend([batch_item_x]*len(batch_y))
-            y.extend(batch_y)
-
-        yield x, y
-
-
-def train_words(words, vocabulary_int):
-    """
-    drop words whose frequency is higher than threshold
-    :return:
-    """
-    words_int = [vocabulary_int[word] for word in words]
-
-    t = 1e-5
-    threshold = 0.8
-
-    words_int_count = Counter(words_int)
-    total_count = len(words_int)
-    # frequency of each word in type of dict
-    word_freq = {w: c / float(total_count) for w, c in words_int_count.items()}
-    # drop words whose frequency is higher than threshold
-    drop_prob = {w: 1 - np.sqrt(t / word_freq[w]) for w in words_int_count}
-
-    train_word = [w for w in words_int if drop_prob[w] < threshold]
-    print("number of train_words: %s" % len(train_word))
-    return train_word
-
-
-def vocabulary_dict(words):
-    """
-    remove duplicate words, and transfer word to int in type of dict
-    :param words:
-    :return:
-    """
-    vocabulary = set(words)
-    vocabulary_int = {w: c for c, w in enumerate(vocabulary)}
-    int_vocabulary = {c: w for c, w in enumerate(vocabulary)}
-
-    print("total words: %s" % len(words))
-    print("unique words: %s" % len(vocabulary))
-    # transfer words to int numbers
-    return vocabulary_int, int_vocabulary
-
-
-def negative_sample(vocabulary_size, embedding_size):
-    softmax_w = tf.Variable(
-        tf.truncated_normal([vocabulary_size, embedding_size], stddev=0.1))
-    softmax_b = tf.Variable(tf.zeros([vocabulary_size]))
-
-    loss = tf.nn.sampled_softmax_loss(softmax_w, softmax_b,
-                                      labels,
-                                      embed, n_sample,
-                                      vocabulary_size)
-    cost = tf.reduce_mean(loss)
-    train_step = tf.train.AdamOptimizer(l_rate).minimize(cost)
-
-    return train_step
+# Add variable initializer.
+init = tf.global_variables_initializer()
 
 
 if __name__ == "__main__":
-    file_name = "enwik9"
-    text = read_file(file_name, size=100000000)
-    words = preprocessor(text)
-    print(words[:20])
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(10000):
+            inputs_batch, labels_batch = generate_batch(words, batch_size)
+            feed_dict = {inputs: inputs_batch, labels: labels_batch}
+            _, cur_loss = sess.run([optimizer, loss], feed_dict=feed_dict)
 
-    vocabulary = set(words)
+            if i % 100 == 0:
+                print("loss: %s" % cur_loss)
 
-    vocabulary_int, int_vocabulary = vocabulary_dict(words)
+        trained_embeddings = embeddings.eval()
+        print(trained_embeddings)
+
+    if trained_embeddings.shape[1] == 2:
+        for i, label in enumerate(unique_words):
+            x, y = trained_embeddings[i, :]
+            plt.scatter(x, y)
+            plt.annotate(label, xy=(x, y), xytext=(5, 2),
+                         textcoords='offset points', ha='right', va='bottom')
+        plt.show()
+
+
