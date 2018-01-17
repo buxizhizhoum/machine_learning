5d22d0db2f7769e1c574844eb2442e64982fd382
buxizhizhoum <mapeaks@126.com>
2018-01-17 17:54:11 +0800

    Add exercise_save_tf_model.py

---
+++ tf/exercise_save_tf_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+save model of tensorflow
+
+the mainly used is the tf.train.Saver() class
+"""
+import tensorflow as tf
+
+
+var_1 = tf.Variable(tf.constant(1.0, shape=[1]), name="var_1")
+var_2 = tf.Variable(tf.constant(1.0, shape=[1]), name="var_2")
+
+init = tf.global_variables_initializer()
+res = tf.add(var_1, var_2)
+
+# this is the core class used when saving model
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(init)
+    saver.save(sess, "/home/buxizhizhoum/tf_model/model_01.ckpt")
+
+
